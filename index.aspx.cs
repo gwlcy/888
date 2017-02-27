d7fa7816c5fba60319ef818a8749d21ca2f26127
18248433866 <309400825@qq.com>
2017-02-27 16:54:03 +0800

    1
    1

---
+++ index.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+
+public partial class index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        File.AppendAllText("clickRate.txt", "1/r/n",Encoding.UTF8);
+    }
+}
